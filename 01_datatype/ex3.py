3931174f847dee14ec2cd31fabb4ee02cb1d9e50
aruba02 <jwdotory@dimigo.hs.kr>
2026-08-28 11:02:13 +0900

    ex3456datatype

---
+++ 01_datatype/ex3.py
@@ -0,0 +1,24 @@
+# bool
+a = True
+print(a, type(a))
+
+print(2 < 3)
+print(2 > 3)
+print(2 == 3)
+print(2 != 3)
+
+print("뽀로로" > "크롱")
+
+print(bool(3))
+print(bool(0))
+print(bool("hello"))
+print(bool(""))
+print(bool([10]))
+print(bool([]))
+
+# None
+a = None
+print(a, type(a))
+
+if a is None:
+    print("Error : No value")
